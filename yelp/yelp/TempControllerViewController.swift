b23ad798ef63bd52732e80463b9097f0f064ad3b
Sanket Patel <sanket@qpme.com>
2014-09-21 15:10:31 -0700

    auto complete in search

--- yelp/yelp/TempControllerViewController.swift
+++ yelp/yelp/TempControllerViewController.swift
@@ -8,36 +8,46 @@
 
 import UIKit
 
-class TempControllerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TempControllerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchDisplayDelegate {
 
-    
-    
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var filterButton: UIBarButtonItem!
+    //@IBOutlet weak var filter2: UIBarButtonItem!
+    @IBOutlet weak var searchBar: UISearchBar!
     
+    var isLoading = false
+    var searchTableView: UITableView!
     let consumerKey = "UqvuPXyQvL7ry65aw6CF2w"
     let consumerSecret = "3Vv_TwZd5x8FYdr_58AO_jXDHhM"
     let accessToken = "JGCPhfIy6DupUtQRvF3gY5z3ZkNFnGl6"
     let accessSecret = "j6-2z91Tq-PgkQrvGU1qZ0FCkKU"
     var yelpClient: YelpClient!
     var searchResults: [SearchResult] = []
+    var filterButton: UIBarButtonItem!
+    var isSearch = false
+    var filteredCategories: [String] = []
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        filterButton.enabled = true
-        self.navigationItem.backBarButtonItem = filterButton
-        
         tableView.delegate = self
         tableView.dataSource = self
+        searchDisplayController?.searchResultsTableView.delegate = self
+        searchDisplayController?.searchResultsTableView.dataSource = self
         self.searchDisplayController?.displaysSearchBarInNavigationBar = true
+        /*
+        let sBar = UISearchBar()
+        sBar.sizeToFit()
+        let barWrapper = UIView(frame: sBar.bounds)
+        barWrapper.addSubview(sBar)
+        self.navigationItem.titleView = barWrapper
+        */
+        
         yelpClient = YelpClient(consumerKey: consumerKey, consumerSecret: consumerSecret, accessToken: accessToken, accessSecret: accessSecret)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "refreshData:" , name: "searchFinished", object: nil)
-        yelpClient.search("food", location: "Fremont", limit:20)
-        
-        
+        yelpClient.search("food", location: "Fremont", limit:10)
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -49,6 +59,12 @@ class TempControllerViewController: UIViewController, UITableViewDelegate, UITab
         
         searchResults = yelpClient.searchResults!
         println("search results : \(searchResults.count)")
+        if (isSearch) {
+            
+            println("search display")
+            
+        }
+        
         tableView.reloadData()
     }
 
@@ -64,15 +80,72 @@ class TempControllerViewController: UIViewController, UITableViewDelegate, UITab
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return searchResults.count
+        return tableView == self.tableView ? searchResults.count : filteredCategories.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        var cell = tableView.dequeueReusableCellWithIdentifier("TempCell") as TempCellTableViewCell
-        var searchResult = searchResults[indexPath.row]
-        cell.populate(searchResult)
-        return cell
+        var isSearch = (tableView == self.tableView) ? false : true
+        if (isSearch) {
+        
+            var cell = UITableViewCell()
+            cell.textLabel?.text = self.filteredCategories[indexPath.row]
+            return cell
+            
+        } else  {
+            
+            var cell = tableView.dequeueReusableCellWithIdentifier("TempCell") as TempCellTableViewCell
+            var searchResult = searchResults[indexPath.row]
+            cell.populate(searchResult)
+            return cell
+        }
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
+        
+        filterCategories(searchString)
+        return true
+    }
+    
+    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+        
+        filterCategories(controller!.searchBar.text)
+        return true
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        var isSearch = (tableView == self.tableView) ? false : true
+        if (isSearch) {
+        
+            var selected = self.filteredCategories[indexPath.row]
+            self.searchDisplayController?.searchBar.text = selected
+            self.searchDisplayController?.searchResultsTableView.hidden = true
+        }
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+
+        var searchFor = searchBar.text
+        println("Search button clicked : \(searchFor)")
+        isSearch = true
+        yelpClient.search(searchFor)
     }
 
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+
+        println("text changed : \(searchText)")
+    }
+
+    func filterCategories(searchText: String) {
+    
+        println("hello : searching for \(searchText)")
+        self.filteredCategories = YelpClient.supportedCategories().filter({
+            (category: String) -> Bool in
+            let stringMatch = category.rangeOfString(searchText, options: nil, range: nil, locale: nil)
+            return stringMatch != nil
+        })
+        
+        println(self.filteredCategories.count)
+    }
 }
